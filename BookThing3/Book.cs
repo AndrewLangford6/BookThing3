7883a2aa1c88c2030a82e9bf2f52fac730cc2539
Andrew Langford <43278589+AndrewLangford6@users.noreply.github.com>
2020-05-01 14:47:09 -0400

    Add project files.

---
+++ BookThing3/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookThing3
+{
+    public class Book
+    {
+        private int refNumber;
+        private string title;
+
+        public Book(int r, string t)
+        {
+            r = refNumber;
+            t = title;
+
+
+        }
+
+    }
+}
---
+++ BookThing3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BookThing3
+{
+    public partial class Form1 : Form
+    {
+
+        List<Book> bookList = new List<Book>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> bookList2 = File.ReadAllLines("BooksFile.txt").ToList();
+
+            for (int i = 0; i < bookList.Count; i += 2)
+            {
+                string title2 = bookList2[i];
+                int number = Convert.ToInt32(bookList[i + 1]);
+                Book hs = new Book(number, title2);
+                bookList.Add(hs);
+            }
+        }
+
+        public Boolean LinearSearch(List<string> searchList, String searchValue)
+        {
+            foreach (string s in searchList)
+            {
+                if (s == searchValue)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
